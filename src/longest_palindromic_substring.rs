6cade893a8c4e6cda821cc693538361a12ca4bc6
Sian Cao <yinshuiboy@gmail.com>
2019-05-11 23:25:38 +0800

    doc: update longest_palindromic_substring

--- src/longest_palindromic_substring.rs
+++ src/longest_palindromic_substring.rs
@@ -7,7 +7,32 @@ pub struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        Solution::dp2(s)
+        Solution::expand_from_center(s)
+    }
+
+    pub fn expand_from_center(s: String) -> String {
+        fn solve(s: &[u8]) -> &[u8] {
+            let mut res = (0, 0);
+            let n = s.len();
+            for c in 0..2*n-1 {
+                let (mut i, mut j) = ((c) / 2, (c+1) / 2);
+                while s[i] == s[j] {
+                    if res.1 - res.0 < j - i {
+                        res = (i, j);
+                    }
+                    if i == 0 || j == n-1 { break }
+                    i -= 1;
+                    j += 1;
+                }
+            }
+
+            &s[res.0..=res.1]
+        }
+
+        if s.len() == 0 { return s };
+
+        let t = solve(s.as_bytes());
+        std::str::from_utf8(t).unwrap().to_string()
     }
 
     // with O(n) space
@@ -168,16 +193,44 @@ mod tests {
     }
 
     #[test]
-    fn test_dp() {
-        assert_eq!(Solution::longest_palindrome("babad".to_string()), "aba");
-        assert_eq!(Solution::longest_palindrome("cbbd".to_string()), "bb");
-        assert_eq!(Solution::longest_palindrome("caba".to_string()), "aba");
-        assert_eq!(Solution::longest_palindrome("ccc".to_string()), "ccc");
-        assert_eq!(Solution::longest_palindrome("".to_string()), "");
+    fn test_expandaround() {
+        assert_eq!(Solution::expand_from_center("babad".to_string()), "bab");
+        assert_eq!(Solution::expand_from_center("cbbd".to_string()), "bb");
+        assert_eq!(Solution::expand_from_center("caba".to_string()), "aba");
+        assert_eq!(Solution::expand_from_center("ccc".to_string()), "ccc");
+        assert_eq!(Solution::expand_from_center("".to_string()), "");
 
         for _ in 0..100{
             let s = gen_random_string();
-            assert!(Solution::is_palindrome(Solution::longest_palindrome(s)));
+            assert!(Solution::is_palindrome(Solution::expand_from_center(s)));
+        }
+    }
+
+    #[test]
+    fn test_dp2() {
+        assert_eq!(Solution::dp2("babad".to_string()), "aba");
+        assert_eq!(Solution::dp2("cbbd".to_string()), "bb");
+        assert_eq!(Solution::dp2("caba".to_string()), "aba");
+        assert_eq!(Solution::dp2("ccc".to_string()), "ccc");
+        assert_eq!(Solution::dp2("".to_string()), "");
+
+        for _ in 0..100{
+            let s = gen_random_string();
+            assert!(Solution::is_palindrome(Solution::dp2(s)));
+        }
+    }
+
+    #[test]
+    fn test_dp() {
+        assert_eq!(Solution::dp1("babad".to_string()), "aba");
+        assert_eq!(Solution::dp1("cbbd".to_string()), "bb");
+        assert_eq!(Solution::dp1("caba".to_string()), "aba");
+        assert_eq!(Solution::dp1("ccc".to_string()), "ccc");
+        assert_eq!(Solution::dp1("".to_string()), "");
+
+        for _ in 0..100{
+            let s = gen_random_string();
+            assert!(Solution::is_palindrome(Solution::dp1(s)));
         }
     }
 
@@ -202,6 +255,14 @@ mod tests {
         b.iter(|| {
             let s = gen_random_string();
             Solution::dp2(s);
+        })
+    }
+
+    #[bench]
+    fn bench_expand(b: &mut Bencher) {
+        b.iter(|| {
+            let s = gen_random_string();
+            Solution::expand_from_center(s);
         })
     }
 }
